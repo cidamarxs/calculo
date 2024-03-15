9c6273a8a156e6f8c20ba7f68fece9eb96a8b81b
cidamarxs <cida.marxs@hotmail.com>
2024-03-15 19:02:22 -0300

    Projeto iniciado com repositório git inserido

---
+++ CalculoRetangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CalculoRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a altura do retângulo:");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a largura do retângulo:");
+            double largura = Double.Parse(Console.ReadLine());
+
+            double diagonal = Math.Sqrt((altura * altura) + (largura * largura));
+            double perimetro = 2 * (altura + largura);
+
+            Console.WriteLine("Altura do retângulo: " + altura);
+            Console.WriteLine("Largura do retângulo: " + largura);
+            Console.WriteLine("Diagonal do retângulo: " + diagonal);
+            Console.WriteLine("Perímetro do retângulo: " + perimetro);
+
+            Console.ReadLine(); // Espera o usuário pressionar Enter antes de fechar
+        }
+    }
